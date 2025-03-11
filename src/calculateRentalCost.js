f1fd4c8ab293e963eabbcbad2e4f33c8445948fd
Ivan Baranovskyi <baranovskyiivan@gmail.com>
2025-03-11 10:51:09 +0100

    calculated rental cost

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,18 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  let total;
+  const costPerDay = 40;
+
+  if (days >= 7) {
+    total = costPerDay * days - 50;
+  } else if (days >= 3) {
+    total = costPerDay * days - 20;
+  } else {
+    total = costPerDay * days;
+  }
+
+  return total;
 }
 
 module.exports = calculateRentalCost;
